46acbe36ddba4eae7458f9001475c1cec6ca320d
Alexander Repeta <alexander.repeta@gmail.com>
2021-07-07 21:28:25 +0300

    add examples

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
+import { differenceInYears } from 'date-fns';
 import 'modern-normalize/modern-normalize.css';
 import './sass/modules/main/index.scss';
+
+const MS_PER_YEAR = 31_556_952_000;
+
+const month = 5;
+const day = 2;
+const year = 1980;
+
+const birthDay = new Date(year, month - 1, day);
+const today = new Date();
+
+console.log(Math.floor((today - birthDay) / MS_PER_YEAR));
+console.log(differenceInYears(today, birthDay));
